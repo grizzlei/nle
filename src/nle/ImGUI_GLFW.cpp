1b187c374aadaf799f521c39e088329fa3d19ea1
Hasan Karaman <hk@hasankaraman.dev>
2023-08-14 14:27:28 +0300

    feat: ImGUI_GLFW class added.

---
+++ src/nle/ImGUI_GLFW.cpp
@@ -0,0 +1,88 @@
+#include "ImGUI_GLFW.h"
+#include "Utils.h"
+
+#include <string>
+
+namespace nle
+{
+
+    ImGUI_GLFW::ImGUI_GLFW(GLFWwindow *handle, unsigned int glsl_version)
+        : m_handle(handle)
+    {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; 
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        ImGui_ImplGlfw_InitForOpenGL(m_handle, true);
+
+        std::string version = "#version " + std::to_string(glsl_version);
+
+        ImGui_ImplOpenGL3_Init(version.c_str());
+        ImGui::StyleColorsDark();
+    }
+
+    ImGUI_GLFW::~ImGUI_GLFW()
+    {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+    }
+
+    void ImGUI_GLFW::render()
+    {
+        // ImGui::Begin("nice little engine", nullptr);
+        // create frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if(ImGui::BeginMainMenuBar())
+        {
+            if(ImGui::BeginMenu("File"))
+            {
+                if(ImGui::MenuItem("Load Model"))
+                {
+                    prdbg("Load Model");
+                }
+                
+                if(ImGui::MenuItem("Quit"))
+                {
+                    prdbg("Quit");
+                }
+                
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMainMenuBar();
+        }
+
+        if(ImGui::Begin("nice little engine", nullptr, ImGuiWindowFlags_MenuBar))
+        {
+            ImGui::TextWrapped("https://github.com/grizzlei/nle");
+            ImGui::TextWrapped("hasan karaman - 2023");
+            ImGui::TextWrapped("experimental opengl renderer and physics engine.");
+            ImGui::Separator();
+            ImGui::End();
+        }
+
+
+        // static bool show_demo_window = true;
+        // ImGui::ShowDemoWindow(&show_demo_window);
+
+        // render
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    }
+
+    bool ImGUI_GLFW::visible() const
+    {
+        return m_visible;
+    }
+
+    void ImGUI_GLFW::set_visible(bool visible)
+    {
+        m_visible = visible;
+    }
+
+} // namespace nle
---
+++ src/nle/ImGUI_GLFW.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <imgui/imgui.h>
+#include <imgui/backends/imgui_impl_glfw.h>
+#include <imgui/backends/imgui_impl_opengl3.h>
+
+#include <GLFW/glfw3.h>
+
+namespace nle
+{
+
+class ImGUI_GLFW
+{
+private:
+    GLFWwindow *m_handle;
+    bool m_visible = true;
+public:
+    ImGUI_GLFW(GLFWwindow *handle, unsigned int glsl_version = 330);
+    ~ImGUI_GLFW();
+    void render();
+
+    bool visible() const;
+    void set_visible(bool visible);
+};
+
+} // namespace nle
