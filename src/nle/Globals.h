9ed9575eb4a86439a5c844081f4afda3f0dd0772
Hasan Karaman <hk@hasankaraman.dev>
2023-09-04 18:27:27 +0300

    Globals added

---
+++ src/nle/Globals.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Shader.h"
+#include "Material.h"
+
+namespace nle::globals
+{
+    extern Shader*      DEFAULT_SHADER;
+    extern Material*    DEFAULT_MATERIAL;
+    
+
+    void init();
+    void cleanup();
+} // namespace nle
