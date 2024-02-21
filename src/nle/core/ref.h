4a34885ad24be5fec7464c838117744e8148e52b
Hasan Karaman <hk@hasankaraman.dev>
2024-02-22 00:00:12 +0300

    feat: weak_ref added.

--- src/nle/core/ref.h
+++ src/nle/core/ref.h
@@ -7,6 +7,9 @@ namespace nle
     template <typename ref_type>
     using ref = std::shared_ptr<ref_type>;
 
+    template <typename weak_ref_type>
+    using weak_ref = std::weak_ptr<weak_ref_type>;
+
     template <typename ref_type, typename ... args_type>
     ref<ref_type> make_ref(args_type&&... args)
     {
