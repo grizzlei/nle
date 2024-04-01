c55abb1c3f6afcf3a891c889fbb58de6268c9264
Hasan Karaman <hk@hasankaraman.dev>
2024-04-02 02:19:10 +0300

    feat: multimesh_instance_3d::render() implemented.

--- src/nle/mesh/multimesh_instance_3d.cpp
+++ src/nle/mesh/multimesh_instance_3d.cpp
@@ -5,21 +5,37 @@ namespace nle
 
     multimesh_instance_3d::multimesh_instance_3d(ref<class multimesh_3d> multimesh)
     {
-        m_multimesh_3d = multimesh;
+        m_multimesh = multimesh;
+
+        for(auto m3d : m_multimesh->meshes())
+        {
+            add_child(make_ref<mesh_instance_3d>(m3d));
+        }
+
+        update();
     }
 
     multimesh_instance_3d::~multimesh_instance_3d()
     {
     }
 
-    ref<class multimesh_3d> multimesh_instance_3d::multimesh_3d()
+    ref<class multimesh_3d> multimesh_instance_3d::multimesh()
     {
-        return m_multimesh_3d;
+        return m_multimesh;
     }
 
     void multimesh_instance_3d::render()
     {
-        throw std::runtime_error("multimesh_instance_3d::render not implemented");
+        for(auto ro : render_objects())
+        {
+            ro->render();
+        }
+    }
+
+    void multimesh_instance_3d::update()
+    {
+        object_3d::update();
+        /// TODO: update model matrix
     }
 
 } // namespace nle
