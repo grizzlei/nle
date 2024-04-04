df095424894aba271b5b845ae799890cde667b1d
Hasan Karaman <hk@hasankaraman.dev>
2024-04-04 04:14:18 +0300

    chore: multiple changes. bad commit.

--- src/nle/object/render_object_3d.h
+++ src/nle/object/render_object_3d.h
@@ -18,11 +18,18 @@
 namespace nle
 {
 
+enum class primitive_type
+{
+    points = GL_POINTS,
+    lines = GL_LINES,
+    triangles = GL_TRIANGLES
+};
+
 enum class render_mode
 {
-    POINT = GL_POINT,
-    LINE = GL_LINE,
-    FILL = GL_FILL 
+    point = GL_POINT,
+    line = GL_LINE,
+    fill = GL_FILL 
 };
 
 enum class render_layer
@@ -51,27 +58,37 @@ public:
     virtual void set_render_layer(enum render_layer rl);
     virtual enum render_layer render_layer() const;
 
+    virtual void set_primitive_type(enum primitive_type pt);
+    virtual enum primitive_type primitive_type();
+
     virtual void set_visible(bool visible);
     virtual bool visible() const;
 
     virtual void set_shader(ref<class shader> shader);
     virtual ref<class shader> shader();
 
-    ref<class object_3d> scene();
+    ref<class render_object_3d> scene();
+
+    std::set<ref<class render_object_3d>, std::owner_less<ref<class render_object_3d>>> render_objects();
+
+    virtual void add_child(ref<object_3d> child) override;
+    virtual void delete_child(ref<object_3d> child) override;
     
     nlohmann::json to_json() const override;
     void from_json(const nlohmann::json& j) override;
     
 private:
     ref<class shader> m_shader;
-    ref<object_3d> m_scene;
+    ref<class render_object_3d> m_scene;
+    std::set<ref<class render_object_3d>, std::owner_less<ref<class render_object_3d>>> m_render_objects;
 
     enum render_layer m_render_layer;
     enum render_mode m_render_mode;
+    enum primitive_type m_primitive_type;
 
     bool m_visible;
 
-    void set_scene(ref<class object_3d> scene);
+    void set_scene(ref<class render_object_3d> scene);
 
     friend class scene_3d;
     friend class renderer_3d;
