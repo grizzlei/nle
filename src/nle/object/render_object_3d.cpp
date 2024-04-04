0e66755c8e1c87d0475dcdb0886a4bd11e78c0ab
Hasan Karaman <hk@hasankaraman.dev>
2024-04-04 04:12:29 +0300

    chore: multiple changes. bad commit.

--- src/nle/object/render_object_3d.cpp
+++ src/nle/object/render_object_3d.cpp
@@ -7,7 +7,8 @@ namespace nle
     {
         m_visible = true;
         m_render_layer = render_layer::_0;
-        m_render_mode = render_mode::FILL;
+        m_render_mode = render_mode::fill;
+        m_primitive_type = primitive_type::triangles;
     }
 
     render_object_3d::~render_object_3d()
@@ -17,6 +18,10 @@ namespace nle
     void render_object_3d::set_render_mode(enum render_mode rm)
     {
         m_render_mode = rm;
+        for(auto ro : m_render_objects)
+        {
+            ro->set_render_mode(rm);
+        }
     }
 
     render_mode render_object_3d::render_mode() const
@@ -27,6 +32,10 @@ namespace nle
     void render_object_3d::set_render_layer(enum render_layer rl)
     {
         m_render_layer = rl;
+        for(auto ro : m_render_objects)
+        {
+            ro->set_render_layer(rl);
+        }
     }
 
     enum render_layer render_object_3d::render_layer() const
@@ -34,9 +43,27 @@ namespace nle
         return m_render_layer;
     }
 
+    void render_object_3d::set_primitive_type(enum primitive_type pt)
+    {
+        m_primitive_type = pt;
+        for(auto ro : m_render_objects)
+        {
+            ro->set_primitive_type(pt);
+        }
+    }
+
+    primitive_type render_object_3d::primitive_type()
+    {
+        return m_primitive_type;
+    }
+
     void render_object_3d::set_visible(bool visible)
     {
         m_visible = visible;
+        for(auto ro : m_render_objects)
+        {
+            ro->set_visible(visible);
+        }
     }
 
     bool render_object_3d::visible() const
@@ -47,12 +74,9 @@ namespace nle
     void render_object_3d::set_shader(ref<class shader> shader)
     {
         m_shader = shader;
-        for(auto child : children())
+        for(auto ro : m_render_objects)
         {
-            if(auto ro = std::dynamic_pointer_cast<render_object_3d>(child))
-            {
-                ro->set_shader(shader);
-            }
+            ro->set_shader(m_shader);
         }
     }
 
@@ -76,20 +100,48 @@ namespace nle
         m_render_mode = static_cast<enum render_mode>(j["render_mode"]);
     }
 
-    void render_object_3d::set_scene(ref<class object_3d> scene)
+    void render_object_3d::set_scene(ref<class render_object_3d> scene)
     {
         m_scene = scene;
-        for(auto child : children())
+        for(auto ro : m_render_objects)
         {
-            if(auto ro = std::dynamic_pointer_cast<render_object_3d>(child))
-            {
-                ro->set_scene(scene);
-            }
+            ro->set_scene(scene);
         }
     }
 
-    ref<class object_3d> render_object_3d::scene()
+    ref<class render_object_3d> render_object_3d::scene()
     {
         return m_scene;
+    }
+
+    std::set<ref<class render_object_3d>, std::owner_less<ref<class render_object_3d>>> render_object_3d::render_objects()
+    {
+        return m_render_objects;
+    }
+
+    void render_object_3d::add_child(ref<object_3d> child)
+    {
+        object_3d::add_child(child);
+
+        if(auto ro = std::dynamic_pointer_cast<render_object_3d>(child))
+        {
+            if(m_render_objects.insert(ro).second)
+            {
+                ro->set_scene(m_scene);
+                ro->set_shader(m_shader);
+                ro->set_visible(m_visible);
+                ro->set_render_layer(m_render_layer);
+                ro->set_primitive_type(m_primitive_type);
+            }
+        }
+    }
+    
+    void render_object_3d::delete_child(ref<object_3d> child)
+    {
+        if(auto ro = std::dynamic_pointer_cast<render_object_3d>(child))
+        {
+            m_render_objects.erase(ro);
+        }
+        object_3d::delete_child(child);
     }
 } // namespace nle
